7c558aba6c2839d32e9588454fdd31d95b665e3f
Damilare Agba <blankgodd33@gmail.com>
2024-12-09 17:43:26 +0100

    [update]: refactoring

--- src/components/modules/permit/permit-management.tsx/activities/process-permit/submit.tsx
+++ src/components/modules/permit/permit-management.tsx/activities/process-permit/submit.tsx
@@ -59,7 +59,8 @@ export default function Submit() {
           acc[`${camelCaseName}Name`] = "..."; // Add the Name field
         }
 
-        acc[`${camelCaseName.replace(/Doc$/i, "")}Type`] = doc.type;
+        // Keep "Doc" in the key names
+        acc[`${camelCaseName}Type`] = doc.type;
         acc[`${camelCaseName}`] = doc.doc;
 
         return acc;
